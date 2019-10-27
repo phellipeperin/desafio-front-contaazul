0211e9650b1e8922b0743001c76728f553e641a1
Phellipe Perin <phellipe.perin@gmail.com>
2019-10-27 10:01:19 -0300

    clear project, config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,42 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-  },
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
-      env: {
-        jest: true,
-      },
+    root: true,
+    env: {
+        node: true,
     },
-  ],
+    extends: [
+        'plugin:vue/recommended',
+        '@vue/airbnb',
+    ],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        indent: [2, 4],
+        'vue/html-indent': [2, 4],
+        'vue/script-indent': [2, 4, {
+            baseIndent: 1,
+        }],
+    },
+    parserOptions: {
+        parser: 'babel-eslint',
+    },
+    overrides: [
+        {
+            files: [
+                '**/__tests__/*.{j,t}s?(x)',
+                '**/tests/unit/**/*.spec.{j,t}s?(x)',
+            ],
+            env: {
+                jest: true,
+            },
+        },
+        {
+            files: ['*.vue'],
+            rules: {
+                indent: 'off',
+            },
+        },
+    ],
+    plugins: [
+        'html',
+    ],
 };
--- README.md
+++ README.md
@@ -1,11 +1,21 @@
-# desafio-front-contaazul
+## Desafio Front-end Conta Azul - Phellipe Perin
+This project is a ContaAzul's front-end challenge implementation in Vue.js by Phellipe Perin. 
 
-## Project setup
+Project's specifications: https://drive.google.com/file/d/1JztMD9cdYhKzQ-GIPxvY1lUVGjWbwH1f/view
+
+The desired layout: https://marvelapp.com/13gd240g/screen/55669566
+
+### Result
+You can found the result of this project here: http://contaazul.phellipe.com.br/
+
+### Setup
 ```
+git clone https://github.com/phellipeperin/desafio-contaazul
 npm install
 ```
 
-### Compiles and hot-reloads for development
+### Compiles for development (hot-reloads)
+Run the following command and access the project on localhost:8080
 ```
 npm run serve
 ```
@@ -15,20 +25,16 @@ npm run serve
 npm run build
 ```
 
-### Run your tests
+### Tests
+This project has only unit tests. Run all of them with the following command:
 ```
 npm run test
 ```
 
-### Lints and fixes files
+#### Lints and fixes files
+I used the Airbnb styleguide, you can check it out here: https://github.com/airbnb/javascript
+
+However, I made some changes to indentation. Don't worry, lint will warn you :)
 ```
 npm run lint
 ```
-
-### Run your unit tests
-```
-npm run test:unit
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
